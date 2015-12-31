0f0a018b128b7fcc0e323dd37ed79fd49ddf02fb
robins35 <caiden.robinson35@gmail.com>
2015-12-31 06:40:09 -0800

    Add rofi and change mod key to unix key

--- config.h
+++ config.h
@@ -43,7 +43,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,11 +56,17 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "terminator", NULL };
+static const char *rofiruncmd[] = { "rofi", "-show", "run", "-location", "6", NULL };
+static const char *rofiwindowcmd[] = { "rofi", "-show", "window", "-location", "6", NULL };
+static const char *rofisshcmd[] = { "rofi", "-show", "ssh", "-location", "6", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofiruncmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = rofiwindowcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = rofisshcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
